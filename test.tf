b48f651ca8f1db41e8bda9ceb0720dd88f3cb542
manishmu <manish.mudholkar@gmail.com>
2019-04-20 08:33:49 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = "/home/manish.aws/credentials"
+  shared_credentials_file = "/home/manish/.aws/credentials"
   profile    = "default"
   region     = "us-east-1"
   }
