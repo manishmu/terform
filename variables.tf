3bf10344ad8f2bc5c22efc8132a94b85cf38b00d
manishmu <manish.mudholkar@gmail.com>
2019-04-20 07:11:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
     default = "us-east-1"
 }
 
-variable "access_key" {AKIA3LR4AZ5NNOIRFPBI}
+variable "access_key" {
+    key= 'AKIA3LR4AZ5NNOIRFPBI'
+    }
 
-variable "secret_key" {d5P6JPyr6NuXvBNqQ0BU9VRtMCg4FTloH6HFcUwS}
+variable "secret_key" {
+    'd5P6JPyr6NuXvBNqQ0BU9VRtMCg4FTloH6HFcUwS'
+}
