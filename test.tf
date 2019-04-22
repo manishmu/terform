8e8bb68fc895c38582875e84dc5f4d2a8a19649c
manishmu <manish.mudholkar@gmail.com>
2019-04-22 06:06:46 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -20,6 +20,12 @@ resource "aws_security_group" "webserver" {
     to_port   = 80
     cidr_blocks = ["0.0.0.0/0"]
     }
+  ingress {
+    protocol = "tcp"
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    }
   egress {
     from_port   = 0
     to_port     = 0
