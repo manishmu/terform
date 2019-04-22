d4d32caaec1ce934fa827d1bc04235ab01c729a6
manishmu <manish.mudholkar@gmail.com>
2019-04-22 05:53:29 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -12,11 +12,27 @@ resource "aws_iam_group_membership" "test" {
   group = "Eng"
   depends_on = ["aws_iam_user.test"]
   }
+resource "aws_security_group" "webserver" {
+  name = "webserver"
+  ingress {
+    protocol = tcp
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  }
 resource "aws_instance" "Web" {
 ami = "ami-0ff8a91507f77f867"
 instance_type = "t2.micro"
 key_name = "EC2_tf_test"
 user_data = "${file("/Storage/terraform-work/install.sh")}"
+vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
 count = "1"
   tags {
     Name = "Webserver"
